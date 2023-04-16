84ac8006a915158d4d82cde3c5395e89b75e14e3
Артём <100671516+qwertyuiopqazwsx@users.noreply.github.com>
2023-04-16 18:04:55 +0600

    Add files via upload

--- auto_folder/main.py
+++ auto_folder/main.py
@@ -1,49 +1,53 @@
 import os
 import time
-import threading
+#import threading
 from modules. class_errors import AutomaticError
 
-MY_DIRECTORY = f"Введи свою директорию сюда!"
+MY_DIRECTORY = f"C:\\Users\\ogurec\\Desktop\\Artyom\\IT"
 
 class Check:
+    """Класс программы. Программа создана для себя, чтобы не мучаться с созданием файлов main.py, start.bat. А также чтобы не открывать VSC.
+    Программа автоматизирует мои действия и то, что иногда может занимать минуту времени"""
     def __init__(self) -> None:
+        """Функция инициализации, отвечает за выбор директории"""
         self.directory = MY_DIRECTORY
         self.check_directory = True
         self.start_animations = False
         self.exit = True
-        while self.exit:
-            while self.check_directory:
-                os.system("cls")
-                self.catalog = os.listdir(path=f'{self.directory}')
-                self.catalog = [i for i in self.catalog  if os.path.isdir(self.directory+"\\"+i)] + ['Вернуться назад', 'Остаться здесь']
-                print('\nВыберите папку | Select a folder:', ''.join([f'\n{i+1}. {folder}' for i, folder in enumerate(self.catalog)]))
-                activity = self.get_user_select_catalog(lenght=len(self.catalog))
-                if activity == False:
+        while self.exit: # если запущено  приложение
+            while self.check_directory: # если запущена проверка директории
+                os.system("cls") # очистка консоли
+                self.catalog = os.listdir(path=f'{self.directory}') # получаем список файлов указанной директории. ~listdir() return list['str']
+                self.catalog = [i for i in self.catalog  if os.path.isdir(self.directory+"\\"+i)] + ['Вернуться назад', 'Остаться здесь'] # сортируем элементы директории, оставляя только каталоги. добавляем два дополнительных действия - вернуться назад и остаться здесь
+                print('\nВыберите папку | Select a folder:', ''.join([f'\n{i+1}. {folder}' for i, folder in enumerate(self.catalog)])) 
+                activity = self.get_user_select_catalog(lenght=len(self.catalog)) # получаем дальнейшую активность
+                if activity == False: # если вернуло 0, то завершаем прогулку по директориям
                     self.check_directory = False
-                elif activity == True:
+                elif activity == True: # если вернуло 1 (вернуться назад) то возвращаемся назад
                     self.del_user_directory()
-            self.create_files()
+            self.create_files() # после завершения прогулки по директориям переход к созданию файлолв
         
 
     def create_files(self):
-        os.system("cls")
-        tasks = ['Создать каталог', 'main.py, start.bat', 'Выбрать другой каталог']
+        """Действие создания файлов и прочего"""
+        os.system("cls") # очиста консоли
+        tasks = ['Создать каталог', 'main.py, start.bat', 'Выбрать другой каталог'] # задачи
         print('\nВыберите что создать:', ''.join([f'\n{i+1}. {task}' for i, task in enumerate(tasks)]))
-        user_select_files = self.get_user_select_files(len(tasks))
+        user_select_files = self.get_user_select_files(len(tasks)) # получаем ответ пользователя
 
         #anim = threading.Thread(target=self.animations, args=())
 
-        if user_select_files == 1:
+        if user_select_files == 1: # Создание нового каталога
             os.system('cls')
             name_new_catalog = str(input('\nВведите имя нового каталога: '))
-            self.start_animations = True
+            #self.start_animations = True
             #anim.start()
             self.directory = f'{self.directory}\\{name_new_catalog}'
             os.mkdir(path=f'{self.directory}')
-            self.start_animations = False
+           # self.start_animations = False
 
-        elif user_select_files == 2:
-            self.start_animations = True
+        elif user_select_files == 2: # Создание main.py, start.bat
+            #self.start_animations = True
             #anim.start()
             
             os.chdir(f'{self.directory}')
@@ -55,9 +59,19 @@ class Check:
                 f.write(f'python main.py\nPAUSE')
             os.rename("start.txt", "start.bat")
 
-            self.start_animations = False
+            try: # запуск VSC, Проводника
+                os.system(f'explorer {self.directory}')
+                os.system('code .')
+                print(f'Запущено: Visual Studio Code; Проводник')
+                time.sleep(3)
+            except: # просто запуск Проводника
+                os.system(f'explorer {self.directory}')
+                print(f'Запущено: Проводник')
+                print('Увы, я запустил бы Visual Studio Code, но у вас его нет.')
+                time.sleep(3)
+            #self.start_animations = False
             
-        elif user_select_files == 3:
+        elif user_select_files == 3: # Вернуться к просмотру директорий
             self.check_directory = True
             return
 
@@ -76,6 +90,7 @@ class Check:
 
 
     def get_user_select_files(self, lenght):
+        """Получаем выбранный пользователем каталог"""
         user_select = int(input('\n'))
         if user_select not in range(lenght+1):
             raise AutomaticError("Введите корректный номер каталога! | Yoy need correct input number catalog")
@@ -83,18 +98,21 @@ class Check:
         
 
     def del_user_directory(self):
+        """Возвращаемся к прошлой директории"""
         change_directory = self.directory.split('\\')[:-1]
         self.directory = '\\'.join(change_directory)
 
     def get_user_select_catalog(self, lenght: int) -> bool:
+        """Получаем нужную директорию, либо дальнейшее действие"""
         user_select = int(input('\n'))
         if user_select not in range(lenght+1):
             raise AutomaticError("Введите корректный номер каталога! | Yoy need correct input number catalog")
-        if lenght-user_select == 1 or lenght-user_select == 0:
+        if lenght-user_select == 1 or lenght-user_select == 0: # если lenght-user_select == 1, вернет 1. но если lenght-user_select == 0 то вернет 0, далее смотреть в инициализации
             return lenght-user_select
         self.directory = self.directory + f'\\{self.catalog[user_select-1]}'
 
     def animations(self):
+        """Анимация, выключена в данный момент"""
         while self.start_animations:
             for i in range(1, 4):
                 os.system("cls")
